4e3a2ef89c64e91bb304885d080c8bfea096ef68
pradeep-hk <35995693+pradeep-hk@users.noreply.github.com>
2026-02-25 14:05:41 +0530

    Expand blocked operational commands in block.cmd

--- block.cmd
+++ block.cmd
@@ -1,2 +1,6 @@
 # Blocked operational command prefixes/patterns for execute_junos_command
 request system reboot
+request system halt
+request system power-cycle
+request system power-off
+request system zeroize
